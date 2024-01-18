808fdd142deddbfb022bb9e8b06b9872420f5a8b
notGoodInNames <danilbren1@gmail.com>
2024-01-18 23:53:33 +0100

    a comment

--- sensor_interface.py
+++ sensor_interface.py
@@ -1,7 +1,7 @@
 import json
 
 
-# TODO implement this method
+# TODO DO NOT implement this method, data is not supposed to be saved to a file, I got a bit confused about how API works when I was talking about it
 def load_to_json(some_data):
     json_data = json.dumps(some_data)
     print(json_data)
