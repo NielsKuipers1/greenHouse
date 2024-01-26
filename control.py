d77c54f62808be9467e427ab2d21f8e7096c78e4
notGoodInNames <danilbren1@gmail.com>
2024-01-26 10:07:40 +0100

    control loop for gantry (not tested)

--- control.py
+++ control.py
@@ -1,6 +1,6 @@
-#import RPi.GPIO as GPIO
+import RPi.GPIO as GPIO
 import numpy as np
-#from RpiMotorLib import RpiMotorLib
+from RpiMotorLib import RpiMotorLib
 import keyboard
 import time
 
@@ -18,7 +18,6 @@ DIR_2 = 0
 STEP_2 = 0
 EN_PIN_2 = 0 # LOW to enable
 
-# TODO: measure actual width and height
 WIDTH = 0.375
 HEIGHT = 0.39
 
@@ -26,21 +25,21 @@ pos = np.array([0.0, 0.0])
 dest = np.array([0.0, 0.0])
 
 #setup and enable motors
-# motor_1 = RpiMotorLib.A4988Nema(DIR_1, STEP_1, (-1,-1,-1), "DRV8825")
-# motor_2 = RpiMotorLib.A4988Nema(DIR_2, STEP_2, (-1,-1,-1), "DRV8825")
+motor_1 = RpiMotorLib.A4988Nema(DIR_1, STEP_1, (-1,-1,-1), "DRV8825")
+motor_2 = RpiMotorLib.A4988Nema(DIR_2, STEP_2, (-1,-1,-1), "DRV8825")
 
-# GPIO.setup(EN_PIN_1, GPIO.OUT)
-# GPIO.setup(EN_PIN_2, GPIO.OUT)
-# GPIO.output(EN_PIN_1,GPIO.LOW)
-# GPIO.output(EN_PIN_2,GPIO.LOW)
+GPIO.setup(EN_PIN_1, GPIO.OUT)
+GPIO.setup(EN_PIN_2, GPIO.OUT)
+GPIO.output(EN_PIN_1,GPIO.LOW)
+GPIO.output(EN_PIN_2,GPIO.LOW)
 
-# def enable_motors(on: bool):
-#     if on:
-#         GPIO.output(EN_PIN_1,GPIO.LOW)
-#         GPIO.output(EN_PIN_2,GPIO.LOW)
-#     else:
-#         GPIO.output(EN_PIN_1,GPIO.UP)
-#         GPIO.output(EN_PIN_2,GPIO.UP)
+def enable_motors(on: bool):
+    if on:
+        GPIO.output(EN_PIN_1,GPIO.LOW)
+        GPIO.output(EN_PIN_2,GPIO.LOW)
+    else:
+        GPIO.output(EN_PIN_1,GPIO.UP)
+        GPIO.output(EN_PIN_2,GPIO.UP)
 
 def _convert_to_rotation(vec: np.ndarray) -> np.ndarray:
     """
@@ -77,8 +76,8 @@ def _move(vec: np.ndarray):
     except ValueError:
         steps_2 = 0 
 
-    # motor_1.motor_go(True if rot[0]>0 else False, steptype="Full", steps=steps_1)
-    # motor_2.motor_go(False if rot[1]>0 else True, steptype="Full", steps=steps_2)
+    motor_1.motor_go(True if rot[0]>0 else False, steptype="Full", steps=steps_1)
+    motor_2.motor_go(False if rot[1]>0 else True, steptype="Full", steps=steps_2)
 
 def control():
     """
@@ -123,4 +122,4 @@ def test_with_keyboard():
         control()
         G.update(pos)
         time.sleep(0.1)
-    # GPIO.cleanup()
+    GPIO.cleanup()
