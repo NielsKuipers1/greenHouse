b62a1db9bc76e30b3e0ca7cc71779c5f7bb098eb
notGoodInNames <danilbren1@gmail.com>
2024-01-26 15:50:08 +0100

    changed direction

--- control.py
+++ control.py
@@ -75,8 +75,8 @@ def _move(vec: np.ndarray):
 
     print(f"rotate {rot}")
     print(f"dir = {True if rot[0]>0 else False}")
-    motor_1.motor_go(True if rot[0]>0 else False, steptype="Full", steps=steps_1)
-    motor_2.motor_go(False if rot[1]>0 else True, steptype="Full", steps=steps_2)
+    motor_1.motor_go(False if rot[0]>0 else True, steptype="Full", steps=steps_1)
+    motor_2.motor_go(True if rot[1]>0 else False, steptype="Full", steps=steps_2)
 
 def control():
     """
