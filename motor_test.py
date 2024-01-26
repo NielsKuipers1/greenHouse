4a7c27c41df1a359965e667a81778bbecfec58ee
notGoodInNames <danilbren1@gmail.com>
2024-01-26 15:14:52 +0100

    test update

--- motor_test.py
+++ motor_test.py
@@ -25,18 +25,20 @@ EN_pin = 24 # enable pin (LOW to enable)
 
 dir2 = 25
 step2 = 8
-EN_pin = 7
+EN_pin2 = 7
 
 # Declare a instance of class pass GPIO pins numbers and the motor type
 mymotortest = RpiMotorLib.A4988Nema(direction, step, (21,21,21), "DRV8825")
 mot2 = RpiMotorLib.A4988Nema(dir2, step2, (21, 21, 21), "DRV8825")
 GPIO.setup(EN_pin,GPIO.OUT) # set enable pin as output
+GPIO.setup(EN_pin2,GPIO.OUT)
 
 ###########################
 # Actual motor control
 ###########################
 #
 GPIO.output(EN_pin,GPIO.LOW) # pull enable to low to enable motor
+GPIO.output(EN_pin2,GPIO.LOW)
 while (True):
     mot2.motor_go(False, # True=Clockwise, False=Counter-Clockwise
                     "Full" , # Step type (Full,Half,1/4,1/8,1/16,1/32)
